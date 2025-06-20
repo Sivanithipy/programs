98b3ff72eaa4f834190f07f59983742f548ba475
Sivanithipy <pyshivanithi@gmail.com>
2025-06-20 18:46:32 +0530

    Pattern 3

---
+++ pattern3.java
@@ -0,0 +1,19 @@
+public class pattern3 {
+     public static void main(String[] args) {
+        int n=5;
+    for (int i = 0; i < n * 2; i++) {
+            if (i < n) {
+                for (int j = 0; j < n - i; j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                for (int j = 0; j <= (i % n); j++) {
+                    System.out.print("* ");
+                }
+            }
+
+            System.out.println();
+}   }
+
+}
+
