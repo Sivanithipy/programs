8bcab239fe308f3151cb1415f49a3ee72eae4a76
Sivanithipy <pyshivanithi@gmail.com>
2025-06-20 18:41:51 +0530

    HillPattern Program

---
+++ pattern1.java
@@ -0,0 +1,22 @@
+public class pattern1 {
+    public static void main(String[] args) {
+        int n = 5;
+        for (int i = 0; i < 2 * n - 1; i++) {
+            if (i < n) {
+                
+                for (int j = 0; j <= i; j++) {
+                    System.out.print("* ");
+                }
+            } else {
+                
+                for (int s = 0; s < i - n + 1; s++) {
+                    System.out.print("  "); 
+                }
+                for (int k = 0; k < 2 * n - 1 - i; k++) {
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
