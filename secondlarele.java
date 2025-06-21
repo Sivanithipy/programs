6c0aab0fa3933aaa9c00fc2a5b4da5084b96cf8f
Sivanithipy <pyshivanithi@gmail.com>
2025-06-21 19:59:21 +0530

    Second Largest Element1

--- secondlarele.java
+++ secondlarele.java
@@ -1,7 +1,7 @@
 public class secondlarele {
     public static void main(String[] args) {
      int[] arr1={4,6,5,3,2,1};
-     int[] arr2={3,5,4};
+     int[] arr2={3,5,5};
      System.out.println(findSecondLargestElement(arr1));
      System.out.println(findSecondLargestElement(arr2));
     }
@@ -17,7 +17,7 @@ public class secondlarele {
                 secmax=max;
                 max=num;
             }
-            else if(secmax<num)
+            else if(secmax<num && num!=max)
             {
                secmax=num;
             }
